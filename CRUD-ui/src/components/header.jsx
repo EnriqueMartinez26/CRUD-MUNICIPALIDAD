342a176449ea825f01a627e315a6c31cea3d1a96
Enrique Leonel Martínez <emartinez.03@hotmail.com>
2024-12-28 12:49:02 -0300

    added-images/updated-header

--- CRUD-ui/src/components/header.jsx
+++ CRUD-ui/src/components/header.jsx
@@ -1,5 +1,6 @@
 const Header = () => (
     <header style={styles.header}>
+      <img src="../images/R.png" style={styles.image} />
       <h1 style={styles.h1}>Task Manager</h1>
     </header>
   );
@@ -11,14 +12,18 @@ const Header = () => (
       alignItems: 'center',
       width: '100%',
       height: '100px',
-      backgroundColor: '#ffffff', 
+      backgroundColor: '#ffffff',
       margin: 0,
       padding: 0,
     },
     h1: {
       margin: 0,
       fontSize: '2.5em',
-      color: '#333333', 
+      color: '#333333',
+    },
+    image: {
+      marginRight: '10px',
+      height: '60px',
     },
   };
   
